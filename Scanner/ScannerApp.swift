b9d06528d6509268a764411da88518cbcc21dd7d
Vincy-Cheng <60846680+Vincy-Cheng@users.noreply.github.com>
2023-11-25 21:22:11 +0800

    Initial Commit

---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by Wing Lam Cheng on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

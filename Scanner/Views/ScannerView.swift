2c087338de372f961369d4c4ec141d251a9e2467
Vincy-Cheng <60846680+Vincy-Cheng@users.noreply.github.com>
2023-12-07 23:06:44 +0800

    Update the display format of scanned items
    1. Display the quantity of the item
    2. Group identical item
    3. Add "Add" and "Minus" icon button

--- Scanner/Views/ScannerView.swift
+++ Scanner/Views/ScannerView.swift
@@ -12,7 +12,7 @@ struct ScannerView: View {
     // TODO: Create the Confirm button to make purchase log
     @Binding var isPresentingScannerView:Bool
     @State var parsedProducts: [Product] = []
-
+    
     private var totalValue: Double {
         parsedProducts.reduce(0) { $0 + $1.value }
     }
@@ -21,10 +21,20 @@ struct ScannerView: View {
         String(format: "%.2f", totalValue)
     }
     
-    private func removeProduct(at index: Int) {
-        parsedProducts.remove(at: index)
+    private func removeProduct(name:String) {
+        if let index = parsedProducts.firstIndex(where: { $0.name == name } )
+        {
+            // Use the index found
+            parsedProducts.remove(at: index)
+        } else {
+            // Handle the case where the name is not found
+            print("Name not found in parsedProducts")
+        }
     }
     
+    private func addProduct(product:Product){
+        parsedProducts.append(Product(value: product.value, name: product.name, owner: product.owner))
+    }
     
     var body: some View{
         VStack{
@@ -38,7 +48,7 @@ struct ScannerView: View {
             
             CodeScannerView(codeTypes: [.qr],scanMode: ScanMode.continuous, completion: { result in
                 if case let .success(code) = result {
-                    self.parsedProducts.append(parseJSON(from: code.string)!)
+                    self.parsedProducts.append(contentsOf:parseJSON(from: code.string)!)
                     
                 }
             })
@@ -50,31 +60,55 @@ struct ScannerView: View {
                 .font(.headline)
                 .padding()
             
+            
             List {
-                ForEach(Array(parsedProducts.enumerated()), id: \.1.id) { index, product in
-                    HStack(){
-                        Text("Item \(index+1)")
-                        VStack(alignment: .leading){
-                            Text("Product Name: \(product.name)")
-                            Text("Owner: \(product.owner)")
-                            
-                            Text("Price: HKD$\(String(format: "%.2f", product.value))")
-                            
-                        }.frame(maxWidth: .infinity)
-                        Button(action: {
-                            removeProduct(at: index)
-                        }) {
-                            Image(systemName: "trash")
+                let groupedProduct = Dictionary(grouping: parsedProducts) { $0.name }
+                
+                ForEach(groupedProduct.sorted(by: { $0.key < $1.key }), id: \.key) { name, product in
+                    
+                    let index = groupedProduct.keys.sorted().firstIndex(of: name) ?? 0
+                    
+                    HStack(alignment: .center){
+                        Text("\(index + 1)")
+                        Spacer().frame(width: 20)
+                        VStack(alignment: .leading) {
+                            Text("Product Name: \(product[0].name)")
+                            Text("Owner: \(product[0].owner)")
+                            Text("Price: HKD$\(String(format: "%.2f", product[0].value))")
+                        }
+                        
+                        Spacer()
+                        // Weird response
+                        // When user tap on any button, it will process all the action
+                        // Need to use onTapGesture instead of action
+                        // https://stackoverflow.com/questions/58514891/two-buttons-inside-hstack-taking-action-of-each-other
+                        Button(action: {}) {
+                            Image(systemName: "plus.circle")
+                                .foregroundColor(.green)
+                        }
+                        .onTapGesture {
+                            addProduct(product: product[0])
+                        }
+                        
+                        Text("\(product.count)")
+                        
+                        Button(action: {}) {
+                            Image(systemName: "minus.circle")
                                 .foregroundColor(.red)
+                        }
+                        .onTapGesture {
+                            removeProduct(name: name)
                         }
                         
                     }
-                    
                 }
             }
+            
             
             Spacer()
         }
         
     }
 }
+
+

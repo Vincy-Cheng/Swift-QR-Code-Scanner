0baaecb1509a293776db5d742a5cf3e54ab9b834
Vincy-fivestones <vincy.c@fivestones.net>
2024-06-12 14:42:10 +0800

    [wip] Add icon to text buttons & pre-insert item

--- Scanner/Views/HomeView.swift
+++ Scanner/Views/HomeView.swift
@@ -55,38 +55,46 @@ struct HomeView: View {
                                 ListOwnerView()
                                 
                             } label: {
-                                Text("Manage Owner")
+                                Label(
+                                    title: { Text("Manage Owner") },
+                                    icon: { Image(systemName: "person") }
+                                )
+                                
                             }
                             NavigationLink {
                                 ListCategoryView()
                                 
                             } label: {
-                                Text("Manage Category")
+                                Label(
+                                    title: { Text("Manage Category") },
+                                    icon: { Image(systemName: "folder") }
+                                )
                             }
                             NavigationLink {
                                 ListItemView()
                                 
                             } label: {
-                                Text("Manage Item")
+                                Label(
+                                    title: { Text("Manage Item") },
+                                    icon: { Image(systemName: "list.bullet.clipboard") }
+                                )
                             }
                             NavigationLink {
                                 GroupedTransactionView()
                                 
                             } label: {
-                                Text("Records")
+                                Label(
+                                    title: { Text("Records") },
+                                    icon: { Image(systemName: "plus.magnifyingglass") }
+                                )
                             }
                             Button(action: {
-                                // Minus action
-//                                let index = selectedItems.firstIndex(of: item)
-//                                if(index != nil){
-//                                    selectedItems.remove(at: index!)
-//                                }
-                                
+                                preInsert()
                             }) {
-                                Text("Pre-insert items")
-//                                Image(systemName: "minus.circle.fill")
-//                                    .foregroundColor(Color(uiColor: UIColor(red: 120/255, green: 108/255, blue: 255/255, alpha: 1)))
-//                                    .font(.system(size: 28))
+                                Label(
+                                    title: { Text("Pre-insert items") },
+                                    icon: { Image(systemName: "cart") }
+                                )
                             }
                             
                         } label: {
@@ -115,7 +123,59 @@ struct HomeView: View {
     }
     
     private func preInsert(){
-//        let items:[ItemData] = [ItemData(name: "", price: 0, quantity: 0, status: ItemStatus.available, imageURL: "", category: Category, owner: Owner)]
+        let preInsertData = [PerInsertData(ownerName: "Wmc",
+                                  items: [PerInsertItemData(name: "透明魷魚掛件", price: 25,categoryName: "Splatoon"),
+                                          PerInsertItemData(name: "Random 透明魷魚掛件", price: 20,categoryName: "Splatoon"),
+                                          PerInsertItemData(name: "透明魷魚掛件(Set)", price: 200,categoryName: "Splatoon"),
+                                          PerInsertItemData(name: "打工磁石貼", price: 30,categoryName: "Splatoon"),
+                                          PerInsertItemData(name: "偶像串串", price: 25,categoryName: "Splatoon"),
+                                          PerInsertItemData(name: "糖果包", price: 35,categoryName: "Splatoon"),
+                                          PerInsertItemData(name: "打工鐳射掛件", price: 25,categoryName: "Splatoon"),
+                                          PerInsertItemData(name: "大集合包(Random透明魷魚掛件x1+打工磁石貼+打工鐳射掛件)", price: 100,categoryName: "Splatoon"),
+                                          PerInsertItemData(name: "打工袋", price: 65,categoryName: "Splatoon"),
+                                          PerInsertItemData(name: "迷宮飯磁石貼", price: 85,categoryName: "迷宮飯"),
+                                         ]),
+                    PerInsertData(ownerName: "y",
+                                  items: [PerInsertItemData(name: "KDA 貼紙（新同舊同價）", price: 25,categoryName: ""),
+                                          PerInsertItemData(name: "Zelda 掛件", price: 70,categoryName: ""),
+                                          PerInsertItemData(name: "Splat 小袋", price: 40,categoryName: ""),
+                                          PerInsertItemData(name: "Splat 3 Poster", price: 20,categoryName: ""),
+                                          PerInsertItemData(name: "Splat 3 明信片組 (冇貼紙）", price: 20,categoryName: ""),
+                                          PerInsertItemData(name: "Splat 3 B6 貼紙", price: 10,categoryName: ""),
+                                          PerInsertItemData(name: "Splat 小袋", price: 40,categoryName: ""),
+                                          PerInsertItemData(name: "Splat Octo  掛件 White + Black", price: 50,categoryName: ""),
+                                          PerInsertItemData(name: "Splat Octo  掛件 Black", price: 30,categoryName: ""),
+                                          PerInsertItemData(name: "Chili 辛俐立牌", price: 70,categoryName: ""),
+                                          PerInsertItemData(name: "烏波組合  （3 隻）", price: 60,categoryName: ""),
+                                          PerInsertItemData(name: "烏波單隻", price: 25,categoryName: ""),
+                                         ]),
+                    PerInsertData(ownerName: "Ge",
+                                  items: [PerInsertItemData(name: "古魯夏立牌", price: 70,categoryName: ""),
+                                          PerInsertItemData(name: "奇樹立牌", price: 70,categoryName: ""),
+                                          PerInsertItemData(name: "Koharu 立牌", price: 70,categoryName: ""),
+                                         ]),
+                    PerInsertData(ownerName: "Chow",
+                                  items: [PerInsertItemData(name: "Holo 飯友立牌", price: 15,categoryName: ""),
+                                          PerInsertItemData(name: "Holo 立牌", price: 45,categoryName: ""),
+                                          PerInsertItemData(name: "Splatoon 掛件扣針", price: 35,categoryName: ""),
+                                          PerInsertItemData(name: "水藝大師飯友立牌", price: 45,categoryName: ""),
+                                          PerInsertItemData(name: "Ina post card", price: 10,categoryName: ""),
+                                          PerInsertItemData(name: "Holo 鎖匙扣", price: 35,categoryName: ""),
+                                         ])
+        ]
+        
+        for data in preInsertData{
+            let owner = OwnerController().preInsertOwner(context: managedObjectContext, name: data.ownerName)
+            
+            for item in data.items{
+                var category = CategoryController().preInsertCategory(context: managedObjectContext, name: item.categoryName)
+                
+                let itemData = ItemData(name: item.name, price: item.price, quantity: 10, status: ItemStatus.available, imageURL: "", category: category, owner: owner)
+                let created = itemController.addItem(context: managedObjectContext, data: itemData)
+                
+            }
+        }
+
     }
 }
 
@@ -131,6 +191,17 @@ struct RoundedCornersShape: Shape {
         )
         return Path(path.cgPath)
     }
+}
+
+struct PerInsertData{
+    let ownerName: String
+    let items:[PerInsertItemData]
+}
+
+struct PerInsertItemData{
+    let name: String
+    let price: Double
+    let categoryName: String
 }
 
 #Preview {
